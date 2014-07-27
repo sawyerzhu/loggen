afd2831053a57f03469d526ca656f0646a18fdbd
sawyerzhu <sawyerzhu@126.com>
2014-07-27 14:10:17 +0800

    fake1 complete

--- fake1.js
+++ fake1.js
@@ -42,51 +42,51 @@ var args = process.argv.slice(2);
 
 // visit box every day, every device
 
-// var startDate = new Date(Date.parse('2014-08-01T00:00:00'));
-// var endDate = new Date(Date.parse('2014-08-31T00:00:00'));
-//
-// while (startDate.isBefore(endDate)) {
-//
-//     var all_user_keys = random_data.get_all_user_info_keys();
-//
-//     for (var m = 0; m < all_user_keys.length; m++) {
-//         var user_info = random_data.random_user_info(all_user_keys[m]);
-//
-//         for (var n = 0; n < user_info.devices.length; n++) {
-//             var device = user_info.devices[n]
-//
-//             for (var i = 0; i < 3; i++) {
-//
-//                 var date = new Date(startDate.toFormat('YYYY-MM-DDTHH:MI:SS'));
-//                 date.addHours(random_data.random_int(8, 32));
-//
-//                 var datas = {
-//                     "timestamp": function(log_format, data, key) {
-//                         return date.toFormat(log_format[key].format);
-//                     },
-//
-//                     "login_name": function() {
-//                         return user_info.sub_email;
-//                     },
-//
-//                     "req_mac": function() {
-//
-//                         return device.mac;
-//                     }
-//                 };
-//
-//                 var app = random_data.random_app('1');
-//
-//                 var logs = log_format.gen_aie_acvitity_path_log(app, random_data.random_app_activity_path(app, 'upload_preview_download'), _.clone(datas));
-//
-//                 sendAieActivityPathLog(logs);
-//             }
-//
-//         }
-//     }
-//
-//     startDate.addDays(1);
-// }
+var startDate = new Date(Date.parse('2014-08-01T00:00:00'));
+var endDate = new Date(Date.parse('2014-08-31T00:00:00'));
+
+while (startDate.isBefore(endDate)) {
+
+    var all_user_keys = random_data.get_all_user_info_keys();
+
+    for (var m = 0; m < all_user_keys.length; m++) {
+        var user_info = random_data.random_user_info(all_user_keys[m]);
+
+        for (var n = 0; n < user_info.devices.length; n++) {
+            var device = user_info.devices[n]
+
+            for (var i = 0; i < 3; i++) {
+
+                var date = new Date(startDate.toFormat('YYYY-MM-DDTHH:MI:SS'));
+                date.addHours(random_data.random_int(8, 24));
+
+                var datas = {
+                    "timestamp": function(log_format, data, key) {
+                        return date.toFormat(log_format[key].format);
+                    },
+
+                    "login_name": function() {
+                        return user_info.sub_email;
+                    },
+
+                    "req_mac": function() {
+
+                        return device.mac;
+                    }
+                };
+
+                var app = random_data.random_app('1');
+
+                var logs = log_format.gen_aie_acvitity_path_log(app, random_data.random_app_activity_path(app, 'upload_preview_download'), _.clone(datas));
+
+                sendAieActivityPathLog(logs);
+            }
+
+        }
+    }
+
+    startDate.addDays(1);
+}
 
 // visit other two SaaS
 
@@ -116,7 +116,7 @@ var args = process.argv.slice(2);
 //             for (var i = 0; i < 1; i++) {
 //
 //                 var date = new Date(startDate.toFormat('YYYY-MM-DDTHH:MI:SS'));
-//                 date.addHours(random_data.random_int(8, 32));
+//                 date.addHours(random_data.random_int(8, 24));
 //
 //                 var datas = {
 //                     "timestamp": function(log_format, data, key) {
@@ -147,45 +147,255 @@ var args = process.argv.slice(2);
 
 //Visit twitter, facebook, youtube random
 
-var startDate = new Date(Date.parse('2014-08-01T00:00:00'));
-var endDate = new Date(Date.parse('2014-08-31T00:00:00'));
+// var startDate = new Date(Date.parse('2014-08-01T00:00:00'));
+// var endDate = new Date(Date.parse('2014-08-31T00:00:00'));
+//
+// while (startDate.isBefore(endDate)) {
+//
+//     var all_user_keys = random_data.get_all_user_info_keys();
+//
+//     for (var m = 0; m < all_user_keys.length; m++) {
+//         var user_info = random_data.random_user_info(all_user_keys[m]);
+//
+//         var device = user_info.devices[1];
+//
+//         var app = random_data.random_app(random_data.random_array_data(['30001', '30002', '30003']));
+//
+//         for (var i = 0; i < 1; i++) {
+//
+//             var date = new Date(startDate.toFormat('YYYY-MM-DDTHH:MI:SS'));
+//             date.addHours(random_data.random_int(8, 24));
+//
+//             var datas = {
+//                 "timestamp": function(log_format, data, key) {
+//                     return date.toFormat(log_format[key].format);
+//                 },
+//
+//                 "req_mac": function() {
+//                     return device.mac;
+//                 },
+//                 req_dn: app.hostname,
+//                 activity: "DESTROY"
+//             };
+//
+//             var gw_log = log_format.gen_gw_log(_.clone(datas));
+//             sendGwLog(gw_log);
+//         }
+//
+//     }
+//
+//     startDate.addDays(1);
+// }
 
-while (startDate.isBefore(endDate)) {
+// Abnormal behavior: Eric access Box 20 times on 2014-08-06
 
-    var all_user_keys = random_data.get_all_user_info_keys();
+// var user_info = random_data.random_user_info('eric@gmail.com');
+//
+// var device = user_info.devices[0];
+//
+// var startDate = new Date(Date.parse('2014-08-06T00:00:00'));
+//
+// var app = random_data.random_app('1');
+//
+// for (var i = 0; i < 17; i++) {
+//
+//      var date = new Date(startDate.toFormat('YYYY-MM-DDTHH:MI:SS'));
+//     date.addHours(random_data.random_int(8, 24));
+//
+//     var datas = {
+//         "timestamp": function(log_format, data, key) {
+//             return date.toFormat(log_format[key].format);
+//         },
+//
+//         "login_name": function() {
+//             return user_info.sub_email;
+//         },
+//
+//         "req_mac": function() {
+//             return device.mac;
+//         }
+//     };
+//
+//     var logs = log_format.gen_aie_acvitity_path_log(app, random_data.random_app_activity_path(app, 'upload'), _.clone(datas));
+//
+//     sendAieActivityPathLog(logs);
+// }
 
-    for (var m = 0; m < all_user_keys.length; m++) {
-        var user_info = random_data.random_user_info(all_user_keys[m]);
+// Abnormal behavior: Simon access OneDrive 30 times on 2014-08-13
 
-        var device = user_info.devices[1];
+// var user_info = random_data.random_user_info('simon@gmail.com');
+//
+// var device = user_info.devices[0];
+//
+// var startDate = new Date(Date.parse('2014-08-13T00:00:00'));
+//
+// var app = random_data.random_app('21');
+//
+// for (var i = 0; i < 30; i++) {
+//
+//     var date = new Date(startDate.toFormat('YYYY-MM-DDTHH:MI:SS'));
+//     date.addHours(random_data.random_int(8, 24));
+//
+//     var datas = {
+//         "timestamp": function(log_format, data, key) {
+//             return date.toFormat(log_format[key].format);
+//         },
+//
+//         "login_name": function() {
+//             return user_info.unsub_emails[0];
+//         },
+//
+//         "req_mac": function() {
+//             return device.mac;
+//         }
+//     };
+//
+//     var logs = log_format.gen_aie_acvitity_path_log(app, random_data.random_app_activity_path(app, 'upload'), _.clone(datas));
+//
+//     sendAieActivityPathLog(logs);
+// }
 
-        var app = random_data.random_app(random_data.random_array_data(['30001', '30002', '30003']));
 
-        for (var i = 0; i < 1; i++) {
+// Abnormal behavior: 2014-08-20 File (nodejs.pdf) access by 6 users(julia@yahoo.com, moshe@yahoo.com, olivia@yahoo.com, john@yahoo.com, daniel@yahoo.com, jacob@yahoo.com) on Box, each user access it 5 times.
 
-            var date = new Date(startDate.toFormat('YYYY-MM-DDTHH:MI:SS'));
-            date.addHours(random_data.random_int(8, 32));
+// var user_infos = [
+//     random_data.random_user_info('julia@gmail.com'),
+//     random_data.random_user_info('moshe@gmail.com'),
+//     random_data.random_user_info('olivia@gmail.com'),
+//     random_data.random_user_info('john@gmail.com'),
+//     random_data.random_user_info('daniel@gmail.com'),
+//     random_data.random_user_info('jacob@gmail.com')
+// ];
+//
+// for (var j = 0; j < user_infos.length; j++) {
+//     var user_info = user_infos[j];
+//
+//     var device = user_info.devices[0];
+//
+//     var startDate = new Date(Date.parse('2014-08-20T00:00:00'));
+//
+//     var app = random_data.random_app('1');
+//
+//
+//     for (var i = 0; i < 5; i++) {
+//
+//         var date = new Date(startDate.toFormat('YYYY-MM-DDTHH:MI:SS'));
+//         date.addHours(random_data.random_int(8, 24));
+//
+//         var datas = {
+//             "timestamp": function(log_format, data, key) {
+//                 return date.toFormat(log_format[key].format);
+//             },
+//
+//             "login_name": function() {
+//                 return user_info.sub_email;
+//             },
+//
+//             "req_mac": function() {
+//                 return device.mac;
+//             },
+//
+//             "objs.name": "nodejs.pdf",
+//
+//             "objs.size": "1024",
+//
+//             "objs.type": "file"
+//         };
+//
+//         var logs = log_format.gen_aie_acvitity_path_log(app, random_data.random_app_activity_path(app, 'preview'), _.clone(datas));
+//
+//         sendAieActivityPathLog(logs);
+//     }
+//
+// }
 
-            var datas = {
-                "timestamp": function(log_format, data, key) {
-                    return date.toFormat(log_format[key].format);
-                },
 
-                "req_mac": function() {
-                    return device.mac;
-                },
-                req_dn: app.hostname,
-                activity: "DESTROY"
-            };
+// Abnormal behavior: 2014-08-11 User Jackson(jackson@gmail.com) access Big file(big_data.zip, 60M) on Box 1 times.
 
-            var gw_log = log_format.gen_gw_log(_.clone(datas));
-            sendGwLog(gw_log);
-        }
+// var user_info = random_data.random_user_info('jackson@gmail.com');
+//
+// var device = user_info.devices[0];
+//
+// var startDate = new Date(Date.parse('2014-08-20T00:00:00'));
+//
+// var app = random_data.random_app('1');
+//
+//
+// for (var i = 0; i < 1; i++) {
+//
+//     var date = new Date(startDate.toFormat('YYYY-MM-DDTHH:MI:SS'));
+//     date.addHours(random_data.random_int(8, 24));
+//
+//     var datas = {
+//         "timestamp": function(log_format, data, key) {
+//             return date.toFormat(log_format[key].format);
+//         },
+//
+//         "login_name": function() {
+//             return user_info.sub_email;
+//         },
+//
+//         "req_mac": function() {
+//             return device.mac;
+//         },
+//
+//         "objs.name": "big_data.zip",
+//
+//         "objs.size": 1024 * 1024 * 60,
+//
+//         "objs.type": "file"
+//     };
+//
+//     var logs = log_format.gen_aie_acvitity_path_log(app, random_data.random_app_activity_path(app, 'upload'), _.clone(datas));
+//
+//     sendAieActivityPathLog(logs);
+// }
 
-    }
-
-    startDate.addDays(1);
-}
+// Abnormal behavior: 2014-08-26 User Jack upload file (business_plan.doc) to Box(jack@gmail.com) and Dropbox(jack@abc.com)
+//
+// var user_info = random_data.random_user_info('jack@gmail.com');
+//
+// var device = user_info.devices[0];
+//
+// var startDate = new Date(Date.parse('2014-08-20T00:00:00'));
+//
+// var app = random_data.random_app('1');
+//
+// var app_onedrive = random_data.random_app('21');
+//
+//
+// for (var i = 0; i < 1; i++) {
+//
+//     var date = new Date(startDate.toFormat('YYYY-MM-DDTHH:MI:SS'));
+//     date.addHours(random_data.random_int(8, 24));
+//
+//     var datas = {
+//         "timestamp": function(log_format, data, key) {
+//             return date.toFormat(log_format[key].format);
+//         },
+//
+//         "login_name": function() {
+//             return user_info.sub_email;
+//         },
+//
+//         "req_mac": function() {
+//             return device.mac;
+//         },
+//
+//         "objs.name": "business_plan.doc",
+//
+//         "objs.size": 60,
+//
+//         "objs.type": "file"
+//     };
+//
+//     var logs = log_format.gen_aie_acvitity_path_log(app, random_data.random_app_activity_path(app, 'upload'), _.clone(datas));
+//     sendAieActivityPathLog(logs);
+//
+//     var logs = log_format.gen_aie_acvitity_path_log(app_onedrive, random_data.random_app_activity_path(app_onedrive, 'upload'), _.clone(datas));
+//     sendAieActivityPathLog(logs);
+//
+// }
 
 udp_client.close();
 redis_client.close();
