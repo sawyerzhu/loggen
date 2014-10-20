82d265283c2ef318931f5f2a20ab8261940475d6
sawyerzhu <sawyerzhu@126.com>
2014-10-20 15:20:20 +0800

    remove duplicate HighTail

--- random_data.js
+++ random_data.js
@@ -177,21 +177,6 @@ var app_id_2_app = {
         "activities": require('./aie/common_activity.js')('user_token')
     },
 
-    "14": {
-        "appname": "HighTail",
-        "hostname": "www.hightail.com",
-        "category": ["storage"],
-        "activity_path": {
-            "login": ['-100000'],
-            "upload": ["-100000", "-100001"],
-            "preview": ["-100000", "-100004"],
-            "delete": ["-100000", "-100003"],
-            "upload_preview_download": ["-100000", "-100001", "-100004", "-100002"],
-            "download": ["-100000", "-100004", "-100002"]
-        },
-        "activities": require('./aie/common_activity.js')('user_token')
-    },
-
     "15": {
         "appname": "Wuala",
         "hostname": "www.wuala.com",
