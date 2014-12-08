65c8968bfbcb010abd0322952cc84d92713c487a
sawyerzhu <sawyerzhu@126.com>
2014-12-08 11:06:10 +0800

    commit

--- fake1.js
+++ fake1.js
@@ -36,7 +36,7 @@ function sendAieActivityPathLog(logs) {
     }
 }
 
-var host = 'localhost';
+var host = 'dev-ds.stratusee.com';
 var udp_client = new udp.udp_client(host, 514);
 var redis_client = new redis.redis_client(host, 6379);
 
@@ -590,7 +590,7 @@ function abnormal_f(startDate) {
     }
 }
 
-[6, 7, 8, 9, 10, 11].forEach(function(month) {
+[/*6, 7, 8, 9, 10, */11].forEach(function(month) {
     var max_day = 30;
 
     if (month == 6 || month == 7 || month == 9 || month == 11) {
